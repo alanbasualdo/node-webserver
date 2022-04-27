162b4186eea4151bf0b29aa38289e38e183d0fe2
Alan Basualdo <alanbasualdo7@outlook.com>
2022-04-27 16:36:09 -0300

    Despliegue app 27-04-22

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 
+app.listen(port, (req, res) => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
+
 // Handlebars
 app.set('view engine', 'hbs')
 hbs.registerPartials(__dirname + '/views/partials')
@@ -12,31 +16,28 @@ hbs.registerPartials(__dirname + '/views/partials')
 // Servir contenido estático
 app.use(express.static('public'))
 
-app.listen(port, (req, res) => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
-
-app.get('/', (req, res) => {
-    res.render('home', {
-        nombre: 'Alan',
-        titulo: 'Node'
-    })
-});
-
-app.get('/elements', (req, res) => {
-    res.render('elements', {
-        nombre: 'Alan',
-        titulo: 'Node'
-    })
-});
-
-app.get('/generic', (req, res) => {
-    res.render('generic', {
-        nombre: 'Alan',
-        titulo: 'Node'
-    })
-});
-
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html')
+    res.sendFile(__dirname + '/public/index.html')
 });
+
+// app.get('/', (req, res) => {
+//     res.render('home', {
+//         nombre: 'Alan',
+//         titulo: 'Node'
+//     })
+// });
+
+// app.get('/elements', (req, res) => {
+//     res.render('elements', {
+//         nombre: 'Alan',
+//         titulo: 'Node'
+//     })
+// });
+
+// app.get('/generic', (req, res) => {
+//     res.render('generic', {
+//         nombre: 'Alan',
+//         titulo: 'Node'
+//     })
+// });
+
